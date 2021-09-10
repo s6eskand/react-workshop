bbcbbc762ab2339f9b92a9722f5d1f74c4a7f687
Sam <sam.eskandar00@hotmail.com>
2021-09-10 17:52:28 -0400

    final touches

--- completed/src/components/ListMovies.js
+++ completed/src/components/ListMovies.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Movie from "./Movie";
-import './ListMovies.css'; // styles for ListMovie component
+import { Search } from 'semantic-ui-react';
+import styles from './ListMovies.module.css'; // styles for ListMovie component
 
 function ListMovies(props) {
+    // initialize state and function to set state using the useState Hook
+    const [movies, setMovies] = useState([]);
+    const [filteredMovies, setFilteredMovies] = useState([]);
+    const [search, setSearch] = useState("");
 
     // calling useEffect on Mount of Component
     useEffect(() => {
@@ -10,25 +15,47 @@ function ListMovies(props) {
         fetch('https://ghibliapi.herokuapp.com/films')
             .then(res => res.json())
             // updates state using setMovies
-            .then(res => setMovies(res));
+            .then(res => {
+                setMovies(res)
+                setFilteredMovies(res)
+            });
     }, []);
 
-    // initialize state and function to set state using the useState Hook
-    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        let filteredMovies = [];
+        if (search.length !== 0) {
+            filteredMovies = [...movies.filter(movie => movie.title.toLowerCase().includes(search))]
+            setFilteredMovies(filteredMovies)
+        } else setFilteredMovies(movies);
+        console.log(search, filteredMovies)
+        // eslint-disable-next-line
+    }, [search])
+
+    const handleSearch = (e) => {
+        setSearch(e.target.value.toString().toLowerCase())
+    }
+
     return (
-        <div className="main">
-            <h1 className="title">Studio Ghibli With React Hooks!</h1>
-            <div className="movies">
+        <div className={styles.main}>
+            <h1 className={styles.title}>Studio Ghibli With React Hooks!</h1>
+            <Search
+                className={styles.search}
+                onSearchChange={handleSearch}
+            />
+            <div className={styles.movies}>
                 {/* Map elements of movies onto Cards */}
-                {movies.map(movie =>
-                    <Movie
-                        // Add Props to Movie Card
-                        classname="movie"
-                        title={movie.title}
-                        description={movie.description}
-                        director={movie.director}
-                        rating={movie.rt_score}
-                    />
+                {filteredMovies.map((movie, idx) =>
+                    <div className={styles.movie}>
+                        <Movie
+                            // Add Props to Movie Card
+                            classname="movie"
+                            title={movie.title}
+                            description={movie.description}
+                            director={movie.director}
+                            rating={movie.rt_score}
+                            key={`${movie.title}-${idx}`}
+                        />
+                    </div>
                 )}
             </div>
         </div>
